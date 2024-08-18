db4cc1bc94b41e1abe08457ba6580b8d837b62c9
Perce Thaveesittikullarp <104191487+Patarakorn@users.noreply.github.com>
2024-08-18 16:42:21 +0700

    Fixed and completed Accounts GET API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,18 +1,17 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
-  const { data: accounts, isLoading } = useGetAccounts();
+  const {data: accounts, isLoading } = useGetAccounts();
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  return (
-    <div>
-      {accounts?.map((account) => (
-        <div key={account.id}>{account.name}</div>
-      ))}
+  return <div>{accounts?.map((account) => (
+    <div key={account.id}>
+      {account.name}
     </div>
-  );
+  ))}</div>;
 }
