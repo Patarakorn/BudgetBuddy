57299d82b2fd42f30985ee1a727d25312ef2a09b
Perce Thaveesittikullarp <104191487+Patarakorn@users.noreply.github.com>
2024-11-10 22:06:46 -0500

    Dashboard Cards

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -101,20 +101,8 @@ const app = new Hono().get(
         value: sql`SUM(ABS(${transactions.amount}))`.mapWith(Number),
       })
       .from(transactions)
-      .innerJoin(
-        accounts,
-        eq(
-          transactions.accountId,
-          accounts.id
-        ),
-      )
-      .innerJoin(
-        categories,
-        eq(
-          transactions.categoryId,
-          categories.id,
-        )
-      )
+      .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+      .innerJoin(categories, eq(transactions.categoryId, categories.id))
       .where(
         and(
           accountId ? eq(transactions.accountId, accountId) : undefined,
@@ -125,14 +113,14 @@ const app = new Hono().get(
         )
       )
       .groupBy(categories.name)
-      .orderBy(desc(
-        sql`SUM(ABS(${transactions.amount}))`
-      ));
+      .orderBy(desc(sql`SUM(ABS(${transactions.amount}))`));
 
     const topCategories = category.slice(0, 3);
     const otherCategories = category.slice(3);
-    const otherSum = otherCategories
-      .reduce((sum, current) => sum + current.value, 0);
+    const otherSum = otherCategories.reduce(
+      (sum, current) => sum + current.value,
+      0
+    );
 
     const finalCategories = topCategories;
     if (otherCategories.length > 0) {
@@ -145,22 +133,20 @@ const app = new Hono().get(
     const activeDays = await db
       .select({
         date: transactions.date,
-        income: sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
-        expenses: sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+        income:
+          sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+            Number
+          ),
+        expenses:
+          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+            Number
+          ),
       })
       .from(transactions)
-      .innerJoin(
-        accounts,
-        eq(
-          transactions.accountId,
-          accounts.id
-        ),
-      )
+      .innerJoin(accounts, eq(transactions.accountId, accounts.id))
       .where(
         and(
-          accountId ? 
-            eq(transactions.accountId, accountId) 
-            : undefined,
+          accountId ? eq(transactions.accountId, accountId) : undefined,
           eq(accounts.userId, auth.userId),
           gte(transactions.date, startDate),
           lte(transactions.date, endDate)
@@ -175,13 +161,13 @@ const app = new Hono().get(
       data: {
         remainingAmount: currentPeriod.remaining,
         remainingChange,
-        incomeAmmount: currentPeriod.income,
+        incomeAmount: currentPeriod.income,
         incomeChange,
         expensesAmount: currentPeriod.expenses,
         expensesChange,
         categories: finalCategories,
         days,
-      }
+      },
     });
   }
 );
